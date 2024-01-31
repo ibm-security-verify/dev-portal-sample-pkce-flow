70a55a1697c98eb15fc573a8187673763aabd5c6
Truman Butterworth <trbutter@au1.ibm.com>
2024-01-31 10:02:45 +1000

    Revert unwanted changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,18 @@
-// "use client";
+"use client";
 import Link from "next/link";
 import React from "react";
 
+export default function Home() {
+  const [authURL, setAuthURL] = React.useState('') 
 
-async function getAuthURL() {
-  const res = await fetch('http://localhost:3000/api/auth/authURL');
-  return res.json();
-}
-export default async function Home() {
-  // const [authURL, setAuthURL] = React.useState('') 
+  React.useEffect(() => {
+    fetch('/api/auth/authURL',{method: 'POST'})
+      .then((res) => res.json())
+      .then((data) => {
+        setAuthURL(data);
+      })
+  }, []);
 
-  // React.useEffect(() => {
-  //   fetch('/api/auth/authURL',{method: 'POST'})
-  //     .then((res) => res.json())
-  //     .then((data) => {
-  //       setAuthURL(data);
-  //     })
-  // }, []);
-  const authURL = await getAuthURL();
-  console.log(authURL)
   return (
     <div>
       <p className="pb-6 pt-4 text-lg">The sample app demonstrates how to authenticate with <span className="font-bold">IBM Security Verify</span> 
