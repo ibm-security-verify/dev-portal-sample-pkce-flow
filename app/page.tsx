1609b3530e84c1e4bb72eb34e129374bc78e7d0a
Truman Butterworth <32148874+trumanbutter@users.noreply.github.com>
2024-02-19 10:41:46 +1000

    Update page.tsx
    Add local loading state to trigger re-render.
    Fixes issue on initial launch where button wasn't rendering until doing a manual refresh.

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,15 @@ import Link from "next/link";
 import React from "react";
 
 export default function Home() {
-  const [authURL, setAuthURL] = React.useState('') 
+  const [authURL, setAuthURL] = React.useState('');
+  const [loading, setIsLoading] = React.useState(true);
   const ref = React.useRef(false);
   React.useEffect(() => {
     if(!ref.current) {
       fetch('/api/auth/authURL',{method: 'POST'})
         .then((res) => res.json())
         .then((data) => {
+          setIsLoading(false);
           setAuthURL(data);
         });
         ref.current = true;
@@ -17,15 +19,17 @@ export default function Home() {
 
   }, []);
 
+  if(loading) {
+    return <p>Loading...</p>
+  }
+
   return (
     <div>
       <p className="pb-6 pt-4 text-lg">The sample app demonstrates how to authenticate with <span className="font-bold">IBM Security Verify</span> 
         <br />using authorization code grant type with (PKCE).
       </p>
       <p className="mb-5 text-lg">Click the login button to start the authentication process.</p>
-      {authURL && (
-        <Link className="bg-blue-600 hover:bg-blue-700 text-white py-2 px-4 rounded" href={authURL}>Login</Link>
-      )}
+      <Link className="bg-blue-600 hover:bg-blue-700 text-white py-2 px-4 rounded" href={authURL}>Login</Link>
     </div>
   );
 }
